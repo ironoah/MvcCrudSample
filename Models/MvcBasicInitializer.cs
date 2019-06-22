47311683f395c39e09756e1bdb53ba335bb3a4cb
natani <ironoah@gmail.com>
2019-06-22 14:27:54 +0900

    Update MvcBasicInitializer.cs

--- Models/MvcBasicInitializer.cs
+++ Models/MvcBasicInitializer.cs
@@ -4,31 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 
-
-namespace MvcCrudSample.Models
+namespace MvcCrudSample.Models 
 {
-    public class MvcBasicInitializer : DropCreateDatabaseAlways<MvcBasicContext>
+    public class MvcBasicInitializer : DropCreateDatabaseAlways<MvcBasicContext> 
     {
-        protected override void Seed(MvcBasicContext context)
+        protected override void Seed (MvcBasicContext context) 
         {
-            var members = new List<Member> {
-        new Member {
-          name = "Adam Smith",
-          email = "smith@msn.com",
-          birth = DateTime.Parse("1970-03-21"),
-          married = false,
-          memo = "tall"
-        },
-        new Member {
-          name = "Kobe Gabriel",
-          email = "Kobe@hotmail.com",
-          birth = DateTime.Parse("1955-01-03"),
-          married = false,
-          memo = "I wish for peace on the earth"
-        }
-      };
-            members.ForEach(m => context.Members.Add(m));
-            context.SaveChanges();
+            var members = new List<Member> 
+            {
+                new Member {
+                name = "Adam Smith",
+                email = "smith@msn.com",
+                birth = DateTime.Parse ("1970-03-21"),
+                married = false,
+                memo = "tall"
+                },
+                new Member {
+                name = "Kobe Gabriel",
+                email = "Kobe@hotmail.com",
+                birth = DateTime.Parse ("1955-01-03"),
+                married = false,
+                memo = "I wish for peace on the earth"
+                }
+            };
+            members.ForEach (m => context.Members.Add (m));
+            context.SaveChanges ();
         }
     }
 }
